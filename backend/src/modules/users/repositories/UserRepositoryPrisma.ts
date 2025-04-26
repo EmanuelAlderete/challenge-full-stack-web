b5f96089c4e04d81a722b6f2355eb2e9f1a34b6f
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-26 13:04:05 -0300

    Enhance user creation process logging in UserRepositoryPrisma by adding initialization and conclusion logs, and improve error handling with detailed error logging.

--- backend/src/modules/users/repositories/UserRepositoryPrisma.ts
+++ backend/src/modules/users/repositories/UserRepositoryPrisma.ts
@@ -2,24 +2,29 @@ import { PrismaClient } from "../../../../prisma/generated/prisma-client-js";
 import { IUserRepository } from "./IUserRepository";
 import { RequestUserDto } from "../dtos/RequestUserDto";
 import * as bcrypt from "bcryptjs";
+import logger from "../../../shared/config/winston.config";
 
 const prisma = new PrismaClient();
 
 export class UserRepositoryPrisma implements IUserRepository {
   async create({ name, email, password }: RequestUserDto) {
     try {
+      logger.info(`Inicializing creation process. [EMAIL: ${email}]`);
       const hashedPassword = await bcrypt.hash(password, 10);
-
-      return await prisma.user.create({
+      const newUser = await prisma.user.create({
         data: {
           name,
           email,
           password: hashedPassword,
         },
       });
-    } catch (error) {
-      console.error("Erro Prisma:", JSON.stringify(error, null, 2));
-      throw new Error("Failed to create user");
+      logger.info(`Creation process concluded. [EMAIL: ${newUser.email}]`);
+      return newUser;
+    } catch (error: any) {
+      logger.error(
+        `Error on USER REPOSITORY during creation process: ${error.message}`,
+        { dbError: error }
+      );
     }
   }
 }
