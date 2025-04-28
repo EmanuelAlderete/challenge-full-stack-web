470980c0397c0f378a12a885fc9b73dbda1c0426
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-28 11:42:59 -0300

    Update IStudentRepository interface to use Partial<RequestStudentDto> in update method

--- backend/src/modules/students/repositories/student.repository.interface.ts
+++ backend/src/modules/students/repositories/student.repository.interface.ts
@@ -4,6 +4,6 @@ export interface IStudentRepository {
   create(data: RequestStudentDto): Promise<any>;
   all(): Promise<any>;
   // getById(id: number): Promise<any>;
-  // update(id: number, data: Partial<RequestUserDto>): Promise<any>;
+  update(id: number, data: Partial<RequestStudentDto>): Promise<any>;
   // delete(id: number): Promise<any>;
 }
