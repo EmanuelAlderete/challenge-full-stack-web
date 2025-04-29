880f8d2b81af4f7b7cbadd1253d827b77653b4c5
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-29 18:16:35 -0300

    refactor: standardize response structure and remove unnecessary logging in StudentsController

--- backend/src/modules/students/controllers/students.cotroller.ts
+++ backend/src/modules/students/controllers/students.cotroller.ts
@@ -19,42 +19,47 @@ export class StudentsController {
   create = async (req: Request, res: Response): Promise<void> => {
     const studentDto: CreateStudentDto = req.body;
     const student = await createStudentUseCase.execute(studentDto);
-    logger.info(`Student created successfully: [Student ID: ${student.id}]`);
-    res.location(`/students/${student.id}`).status(201).json(student);
+    res.location(`/students/${student.id}`).status(201).json({
+      success: true,
+      message: "Estudando criado com sucesso!",
+      data: student,
+    });
   };
 
   index = async (req: Request, res: Response): Promise<void> => {
     const studensList = await listStudentsUseCase.execute();
-    res.status(200).json(studensList);
+    res.status(200).json({
+      success: true,
+      data: studensList,
+    });
   };
 
   update = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
     const studentDto: CreateStudentDto = req.body;
     const student = await updateStudentUseCase.execute(Number(id), studentDto);
-    res.location(`/students/${student.id}`).status(200).json(student);
+    res.location(`/students/${student.id}`).status(200).json({
+      success: true,
+      message: "Estudando atualizado com sucesso!",
+      data: student,
+    });
   };
 
   delete = async (req: Request, res: Response): Promise<void> => {
-    logger.info(`Received request to delete student, [ID - ${req.params.id}]`);
-    try {
-      const { id } = req.params;
-      await deleteStudentUseCase.execute(Number(id));
-      logger.info(`Student delete successfully!`);
-      res
-        .status(200)
-        .json({ status: "sucess", message: "Student deleted successfully!" });
-    } catch (error: any) {
-      logger.error(`Error during student deleting process: ${error.message}`, {
-        error,
-      });
-      res.status(500).json({ message: "Failed to delete student" });
-    }
+    const { id } = req.params;
+    await deleteStudentUseCase.execute(Number(id));
+
+    res
+      .status(200)
+      .json({ success: true, message: "Estudante deletado com sucesso!" });
   };
 
   getById = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
     const student = await getStudentByIdUseCase.execute(Number(id));
-    res.status(200).json(student);
+    res.status(200).json({
+      success: true,
+      data: student,
+    });
   };
 }
