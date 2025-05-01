a48525a13fa84b51067fd8d691bf89430cffacfc
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-05-01 16:36:26 -0300

    feat: integrate Dashboard layout into Students page and remove redundant code

---
+++ frontend/src/layouts/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout>
+    <v-app-bar title="Grupo A +">
+      <template #prepend>
+        <v-app-bar-nav-icon @click="isSidebarOpen = !isSidebarOpen" />
+      </template>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item title="Sair" prepend-icon="mdi-logout" link />
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <Sidebar v-model:is-drawer-open="isSidebarOpen" />
+
+    <v-main>
+      <v-container fluid>
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const isSidebarOpen = ref(false);
+</script>
--- frontend/src/pages/Students.vue
+++ frontend/src/pages/Students.vue
@@ -1,45 +1,30 @@
 <template>
-  <v-app-bar title="Grupo A +">
-    <template #prepend>
-      <v-app-bar-nav-icon @click="isSidebarOpen = !isSidebarOpen" />
-    </template>
-  </v-app-bar>
-
-  <Sidebar v-model:is-drawer-open="isSidebarOpen" />
-
-  <v-main class="d-flex justify-center">
+  <Dashboard>
     <v-container>
-      <v-container>
-        <v-row class="justify-center align-center">
-          <v-col
-            class="d-flex align-center justify-space-between"
-            cols="12"
-            lg="12"
-            md="12"
-            sm="12"
+      <v-row class="justify-center align-center">
+        <v-col class="d-flex justify-start" cols="12" lg="12" md="12" sm="12">
+          <v-text-field
+            append-inner-icon="mdi-magnify"
+            class="align-self-center"
+            label="Pesquisar estudante"
           >
-            <v-text-field
-              append-inner-icon="mdi-magnify"
-              class="align-self-center"
-              label="Pesquisar estudante"
-            >
-              <template #append>
-                <student-form-dialog />
-              </template>
-            </v-text-field>
-          </v-col>
-        </v-row>
-        <v-divider />
-      </v-container>
-      <v-container>
-        <v-row class="justify-center align-center">
-          <students-table />
-        </v-row>
-      </v-container>
+            <template #append>
+              <student-form-dialog />
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+      <v-divider />
     </v-container>
-  </v-main>
+    <v-container>
+      <v-row class="justify-center align-center">
+        <students-table />
+      </v-row>
+    </v-container>
+  </Dashboard>
 </template>
 <script setup>
+import Dashboard from "@/layouts/Dashboard.vue";
 import { ref } from "vue";
 
 const isSidebarOpen = ref(false);
