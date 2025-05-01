7a8481e6636e8ad5f3755164ce8bcf72b8bed3bf
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-05-01 16:10:05 -0300

    feat: add Sidebar component with navigation for students

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-navigation-drawer app :model-value="isDrawerOpen">
+    <v-list nav>
+      <v-list-item link title="Estudantes" />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+defineProps({
+  isDrawerOpen: Boolean,
+});
+</script>
